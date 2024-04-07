dfdb6490300482c2365d37f93deff6ce3c8bf740
olaniyi <arokoyueb11@gmail.com>
2024-04-07 20:44:01 +0100

    minor commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import './footer.css'
 
 const Footer = () => {
   return (
-    <div>
-          my 
-    </div>
+    <footer>
+      <div className='divider'></div>
+      <div className='sub-footer-container'>
+        <div>
+          <span>$All4One</span>
+          <p>All4One is a user-friendly platform that integrates the power of AI and blockchain in <br /> a
+seamless and accessible way. This unified platform empowers both individuals <br /> and
+businesses to leverage the combined potential of these technologies without <br /> being bogged
+down by technical complexities.</p>
+        </div> 
+
+        <div>
+          <li>Contact Us</li>
+          <li>Buy $All4Ones</li>
+          <li>Whitepaper</li>
+        </div> 
+        
+        <div>
+          <img src="/icons8-telegram(2).svg" alt="icon" />
+          <img src="./icons8-twitterx.svg" alt="icon" />
+        </div> 
+        </div> 
+
+        </footer>
   )
 }
 
